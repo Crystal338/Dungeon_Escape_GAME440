596b3b957a8ef4a76ae6a1f2ea30e318416d9f5a
jpeterman123 <tomdettling@gmail.com>
2023-04-28 21:38:49 -0400

    Scripts and Level
    Update to patrol script to maintain direction
    Modeled and added rats for environment

--- Dungeon_Escape/Assets/Scripts/PatrolNPC.cs
+++ Dungeon_Escape/Assets/Scripts/PatrolNPC.cs
@@ -7,15 +7,22 @@ public class PatrolNPC : MonoBehaviour
     public Transform[] waypoints;
     private int _currentWaypointIndex = 0;
     public float _speed = 2f;
+    private Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = (transform.position - lastPosition).normalized;
+        if (direction.magnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        lastPosition = transform.position;
         Transform wp = waypoints[_currentWaypointIndex];
         if (Vector3.Distance(transform.position, wp.position) < 0.01f)
         {
